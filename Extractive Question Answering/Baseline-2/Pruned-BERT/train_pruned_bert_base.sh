58db59c3b641ffab078928d9120cc05f30c89e02
rohitrai11 <35056242+rohitrai11@users.noreply.github.com>
2024-09-10 12:45:58 +0530

    Add files via upload

---
+++ Extractive Question Answering/Baseline-2/Pruned-BERT/train_pruned_bert_base.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set environment variables
+SEED="42"
+GPU_ID="0"
+RUN_NAME="pruned_bert_seed${SEED}"
+PJ_NAME="pruned_bert"
+SQuAD_DIR="squad_dataset"
+RE_EXQA_OUT_DIR="output"
+
+# Run the Python script
+CUDA_VISIBLE_DEVICES=$GPU_ID nohup python -u run_squad.py --project $PJ_NAME \
+--model_type pruned-bert \
+--model_name_or_path Intel/bert-base-uncased-sparse-1_2 --do_lower_case \
+--do_train --do_eval --output_dir $RE_EXQA_OUT_DIR/$RUN_NAME --warmup_ratio 0.1 --num_train_epochs 3 --logging_train_steps 1000 --log_before_train --evaluate_during_training --overwrite_output_dir --threads 4 --version_2_with_negative \
+--train_file $SQuAD_DIR/train-v2.0.json \
+--predict_file $SQuAD_DIR/dev-v2.0.json \
+--seed $SEED > log/$RUN_NAME.txt &
+
+echo "Script is running in the background. Check log/$RUN_NAME for output."
