c1d6c5383b3e78968d84b7f262a8b7701ef01240
rohitrai11 <35056242+rohitrai11@users.noreply.github.com>
2024-09-13 15:58:56 +0530

    Add files via upload

---
+++ Extractive Question Answering/Baseline-4/train_random.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set environment variables
+SEED="42"
+GPU_ID="0"
+RUN_NAME="random_layers_remove_bert_squad${SEED}"
+PJ_NAME="random_layers_v2"
+SQuAD_DIR="squad_dataset"
+RE_EXQA_OUT_DIR="output"
+
+# Run the Python script
+CUDA_VISIBLE_DEVICES=$GPU_ID nohup python -u run_squad_random.py --project $PJ_NAME \
+--model_type bert_base \
+--model_name_or_path google-bert/bert-base-uncased --do_lower_case \
+--do_train --do_eval --output_dir $RE_EXQA_OUT_DIR/$RUN_NAME --warmup_ratio 0.1 --num_train_epochs 3 --logging_train_steps 1000 --log_before_train --evaluate_during_training --overwrite_output_dir --threads 4 --version_2_with_negative \
+--train_file $SQuAD_DIR/train-v2.0.json \
+--predict_file $SQuAD_DIR/dev-v2.0.json \
+--seed $SEED > log/$RUN_NAME.txt &
+
+echo "Script is running in the background. Check log/$RUN_NAME for output."
+
+
